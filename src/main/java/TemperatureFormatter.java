76341eda83bb8e57550c221b9ffc0cfa4999e79b
dmIvanchenko <ivanchenko.contact@gmail.com>
2022-01-05 12:37:14 +0200

    final

--- src/main/java/TemperatureFormatter.java
+++ src/main/java/TemperatureFormatter.java
@@ -5,9 +5,7 @@ public class TemperatureFormatter {
         if (str.charAt(0)!='+' && str.charAt(0)!= '-' && str.charAt(str.length()-1)!= 'C'){
             System.out.println("Неправильный формат ввода. необходимо ввести строку в формате +NС\n"
             + "где N - абсолютное значение температуры, + или - являются обязательными параметрами");
-            Scanner scanner = new Scanner(System.in);
-            str = scanner.nextLine();
-            formatChecker(str, t);
+            System.exit(0);
         } else {
             t = formatter(str);
         }
@@ -15,14 +13,23 @@ public class TemperatureFormatter {
     }
 
     public double formatter(String str){
-
+        double temperature = 0;
         if (str.charAt(0) == '+'){
             str = removeCharAt(str, 0);
             str = removeCharAt(str, str.length()-1);
-        } else{
+        } else if(str.charAt(0)=='-'){
             str = removeCharAt(str, str.length()-1);
         }
-        double temperature = Double.parseDouble(str);
+        String regex = "[-+]?\\d+";
+        if (str.matches(regex)){
+            temperature = Double.parseDouble(str);
+
+        }else {
+            double t = 0;
+            System.out.println("Неправильный формат ввода. необходимо ввести строку в формате +NС\n"
+                    + "где N - абсолютное значение температуры, + или - являются обязательными параметрами");
+            System.exit(0);
+        }
         return temperature;
     }
     public static String removeCharAt(String s, int pos) {
